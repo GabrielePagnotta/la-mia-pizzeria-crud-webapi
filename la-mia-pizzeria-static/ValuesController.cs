f9b7e4ec1adb4c48756602fb763cbbee2a341533
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-04-18 14:34:11 +0200

    fin

---
+++ la-mia-pizzeria-static/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static
+{
+    [Route("api/Pizza")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult GetPizzas()
+        {
+            using (DbPizzaContext context = new DbPizzaContext())
+            {
+                IQueryable<Pizza> Pizzas = context.Pizzas;
+                return Ok(Pizzas.ToList());
+            }
+        }
+    }
+}
